a7d1a15bbdbab88575802f9522dae7837534d454
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-03 21:11:50 +0000

    add story for header component

---
+++ stories/Header.stories.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Header } from '../src';
+import testImage from './images/storybook-header-bg.png';
+
+export default {
+	component: Header,
+	title: 'Components/Header',
+};
+  
+const Template = args => {
+	return <React.Fragment>
+		<Header {...args} >
+			<div style={{ display: 'flex', justifyContent: 'center', width: '100%', color: '#000000' }}>
+      	This component is designed to act as a header component for your web applications UI. You can render any content you wish inside the header.
+    	</div>
+		</Header>
+		{
+			args.imageSrc !== undefined ?
+				<div style={{ display: 'flex', justifyContent: 'center', width: '100%', paddingTop: '20px' }}>
+        	<span>Photo by <a href="https://unsplash.com/@huper?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Huper by Joshua Earle</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
+      	</div> : null
+		}
+	</React.Fragment>;
+}
+
+export const Default = Template.bind({});
+Default.args = {
+	id: 'default-header-id',
+};
+
+export const WithTopBorder = Template.bind({});
+WithTopBorder.args = {
+	backgroundColour: 'grey',
+	id: 'header-with-top-border-id',
+	topBorder: 'red'
+};
+
+export const WithBackgroundImage = Template.bind({});
+WithBackgroundImage.args = {
+	id: 'header-id',
+	imageId: 'header-with-image-id',
+	imageSrc: testImage
+};
