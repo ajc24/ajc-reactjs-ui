9b10e879c9a293f31245b9637d024d5ad21c8894
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-03 22:26:01 +0000

    set header image to load gracefully

--- src/ui-elements/supporting-elements/HeaderBackgroundImage.js
+++ src/ui-elements/supporting-elements/HeaderBackgroundImage.js
@@ -5,13 +5,22 @@ import '../../styling/ui-styles.css';
 class HeaderBackgroundImage extends React.Component {
   constructor(props) {
     super(props);
+    this.setImageLoaded = this.setImageLoaded.bind(this);
+  }
+
+  /**
+   * Sets the image as loaded and transitions it into the UI
+   */
+  setImageLoaded() {
+    const imageElement = document.querySelector(`img[id="${this.props.id}"]`);
+    imageElement.style.opacity = 1;
   }
 
   render() {
     const imageStyling = 'ajc-header-image';
     return (
       <React.Fragment>
-        <img id={this.props.id} className={imageStyling} src={this.props.imageSrc} role="presentation" />
+        <img id={this.props.id} className={imageStyling} src={this.props.imageSrc} role="presentation" onLoad={this.setImageLoaded} />
       </React.Fragment>
     );
   }
