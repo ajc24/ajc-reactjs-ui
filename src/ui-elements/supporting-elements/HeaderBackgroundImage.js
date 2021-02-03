154ce83ba66e7ab4651afd075b116e8b3d7d03e1
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-03 21:07:36 +0000

    add header background image component

---
+++ src/ui-elements/supporting-elements/HeaderBackgroundImage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../../styling/ui-styles.css';
+
+class HeaderBackgroundImage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const imageStyling = 'ajc-header-image';
+    return (
+      <React.Fragment>
+        <img id={this.props.id} className={imageStyling} src={this.props.imageSrc} alt="undefined header background image data / path" />
+      </React.Fragment>
+    );
+  }
+}
+HeaderBackgroundImage.propTypes = {
+  /** The unique identifier for the component. */
+  id: PropTypes.string.isRequired,
+  /** The image to be displayed within the component. */
+  imageSrc: PropTypes.oneOfType([ PropTypes.string, PropTypes.object ]).isRequired,
+};
+export default HeaderBackgroundImage;
