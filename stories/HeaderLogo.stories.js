ca82889a38a2494fe9c96d3346b82f6f5d5edf4e
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-04 12:35:57 +0000

    add story for header logo

---
+++ stories/HeaderLogo.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Header, HeaderLogo } from '../src';
+import testImage from './images/storybook-header-logo.png';
+
+export default {
+	component: HeaderLogo,
+	title: 'Components/HeaderLogo',
+};
+  
+const Template = args => {
+	return <React.Fragment>
+		<HeaderLogo {...args} />
+	</React.Fragment>;
+}
+
+export const Default = Template.bind({});
+Default.args = {
+	id: 'header-logo-id',
+	imageSrc: testImage
+};
