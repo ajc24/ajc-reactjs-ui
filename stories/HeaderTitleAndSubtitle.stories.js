9680ee13ec82513adbd7dbf905416daf9e1b3ddc
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-05 16:33:28 +0000

    add story for header title and subtitle component

---
+++ stories/HeaderTitleAndSubtitle.stories.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { HeaderTitleAndSubtitle } from '../src';
+
+export default {
+	component: HeaderTitleAndSubtitle,
+	title: 'Components/HeaderTitleAndSubtitle',
+};
+  
+const Template = args => {
+	return <React.Fragment>
+		<HeaderTitleAndSubtitle {...args} />
+	</React.Fragment>;
+}
+
+export const Default = Template.bind({});
+Default.args = {
+	id: 'header-title-and-subtitle-default-id',
+  subtitleTextContent: 'The subtitle text content is displayed here',
+  titleTextContent: 'Web Application Title'
+};
+
+export const BlackOutlineWhiteText = Template.bind({});
+BlackOutlineWhiteText.args = {
+	id: 'header-title-and-subtitle-black-outline-white-text-id',
+  subtitleTextColour: 'white',
+  subtitleTextContent: 'The subtitle text content is displayed here',
+  subtitleTextOutline: 'black',
+  titleTextColour: 'white',
+  titleTextContent: 'Web Application Title',
+  titleTextOutline: 'black'
+};
+
+export const WithHeadingIndent = Template.bind({});
+WithHeadingIndent.args = {
+  headingIndent: 150,
+	id: 'header-title-and-subtitle-with-heading-indent-id',
+  subtitleTextContent: 'The subtitle text content is displayed here',
+  titleTextContent: 'Web Application Title'
+};
