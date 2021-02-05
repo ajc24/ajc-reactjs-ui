a3416cfc8703f18f260ad898aaa41753c5548a0f
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-05 22:28:13 +0000

    add snapshots for main content panel

---
+++ tests/snapshots/MainContentPanel.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { TestDev } from 'ajc-jest-enzyme';
+import { MainContentPanel } from '../../src';
+
+describe('MainContentPanel', () => {
+  describe('Default props and rendering - Component with white background', () => {
+    let jsonSnapshot;
+
+    beforeAll(() => {
+      jsonSnapshot = TestDev.createSnapshot(
+        <React.Fragment>
+          <MainContentPanel title="Test Title">
+            This is a test.
+          </MainContentPanel>
+        </React.Fragment>
+      );
+    });
+    
+    it('verifies the snapshot for the component', () => {
+      expect(jsonSnapshot).toMatchSnapshot();
+    });
+  });
+
+  describe('Transferred props and rendering - Component with grey background', () => {
+    let jsonSnapshot;
+
+    beforeAll(() => {
+      jsonSnapshot = TestDev.createSnapshot(
+        <React.Fragment>
+          <MainContentPanel title="Test Title" colour="grey">
+            This is a test.
+          </MainContentPanel>
+        </React.Fragment>
+      );
+    });
+
+    it('verifies the snapshot for the component', () => {
+      expect(jsonSnapshot).toMatchSnapshot();
+    });
+  });
+
+  describe('Transferred props and rendering - Component with transparent background', () => {
+    let jsonSnapshot;
+
+    beforeAll(() => {
+      jsonSnapshot = TestDev.createSnapshot(
+        <React.Fragment>
+          <MainContentPanel title="Test Title" colour="transparent">
+            This is a test.
+          </MainContentPanel>
+        </React.Fragment>
+      );
+    });
+
+    it('verifies the snapshot for the component', () => {
+      expect(jsonSnapshot).toMatchSnapshot();
+    });
+  });
+});
