cb680bb442bdec01d77c19a70a19ecf90687f853
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-05 22:28:33 +0000

    add accessibility tests for main content panel

---
+++ tests/accessibility/MainContentPanel.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { toHaveNoViolations } from 'jest-axe';
+import { AccessibilityDev } from 'ajc-accessibility';
+import { TestDev } from 'ajc-jest-enzyme';
+import { MainContentPanel } from '../../src';
+
+describe('Menu', () => {
+  /* Create the test data for the menu */
+  let componentDidMountSpy;
+  const testTimeout = AccessibilityDev.getMaximumTimeout();
+
+  /* Extend the expect behaviour of jest */
+  expect.extend(toHaveNoViolations);
+
+  beforeAll(() => {
+    componentDidMountSpy = jest
+      .spyOn(MainContentPanel.prototype, 'componentDidMount')
+      .mockImplementation(() => {});
+  });
+
+  afterAll(() => {
+    componentDidMountSpy.mockRestore();
+  });
+
+  describe('Default props and rendering - Component with white background', () => {
+    let jestAxeResults;
+
+    beforeAll(async () => {
+      const html = TestDev.mountHtmlTemplate(
+        <React.Fragment>
+          <MainContentPanel title="Test Title">
+            This is a test.
+          </MainContentPanel>
+        </React.Fragment>
+      );
+      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+    }, testTimeout);
+
+    it('verifies the jest-axe accessibility standards for the component', () => {
+      expect(jestAxeResults).toHaveNoViolations();
+    });
+  });
+
+  describe('Transferred props and rendering - Component with grey background', () => {
+    let jestAxeResults;
+
+    beforeAll(async () => {
+      const html = TestDev.mountHtmlTemplate(
+        <React.Fragment>
+          <MainContentPanel title="Test Title" colour="grey">
+            This is a test.
+          </MainContentPanel>
+        </React.Fragment>
+      );
+      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+    }, testTimeout);
+
+    it('verifies the jest-axe accessibility standards for the component', () => {
+      expect(jestAxeResults).toHaveNoViolations();
+    });
+  });
+
+  describe('Transferred props and rendering - Component with transparent background', () => {
+    let jestAxeResults;
+
+    beforeAll(async () => {
+      const html = TestDev.mountHtmlTemplate(
+        <React.Fragment>
+          <MainContentPanel title="Test Title" colour="transparent">
+            This is a test.
+          </MainContentPanel>
+        </React.Fragment>
+      );
+      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+    }, testTimeout);
+
+    it('verifies the jest-axe accessibility standards for the component', () => {
+      expect(jestAxeResults).toHaveNoViolations();
+    });
+  });
+});
