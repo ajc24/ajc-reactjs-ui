bfcdd4e80d63a25f73e8879a19d986cbe9f60161
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-05 17:17:28 +0000

    add colour choice properties

--- src/ui-elements/SmallMenuIcon.js
+++ src/ui-elements/SmallMenuIcon.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../styling/ui-styles.css';
 
 /**
  * The small menu icon can be used to highlight smaller menu items or links in your application UI. This icon is developed from pure CSS.
+ * 
+ * You can render this icon in a choice of colours: black or white.
  */
 class SmallMenuIcon extends React.Component {
   constructor(props) {
@@ -11,8 +14,15 @@ class SmallMenuIcon extends React.Component {
 
   render() {
     const containerStyle = 'ajc-small-menu-icon-container ajc-default';
-    const upperIconStyle = 'ajc-small-menu-icon-upper';
-    const lowerIconStyle = 'ajc-small-menu-icon-lower';
+    let upperIconStyle = 'ajc-small-menu-icon-upper';
+    let lowerIconStyle = 'ajc-small-menu-icon-lower';
+    if (this.props.colour === 'white') {
+      upperIconStyle += ' ajc-background-white';
+      lowerIconStyle += ' ajc-background-white';
+    } else {
+      upperIconStyle += ' ajc-background-black';
+      lowerIconStyle += ' ajc-background-black';
+    }
     return (
       <div role="presentation" className={containerStyle}>
         <div className={upperIconStyle}></div>
@@ -22,4 +32,11 @@ class SmallMenuIcon extends React.Component {
     );
   }
 }
+SmallMenuIcon.propTypes = {
+  /** The colour of the small icon */
+  colour: PropTypes.oneOf([ 'white', 'black' ]),
+};
+SmallMenuIcon.defaultProps = {
+	colour: 'black'
+};
 export default SmallMenuIcon;
--- src/ui-elements/SmallRightArrowIcon.js
+++ src/ui-elements/SmallRightArrowIcon.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../styling/ui-styles.css';
 
 /**
  * The small right arrow icon can be used to highlight smaller links or buttons in your application UI. This icon is developed from pure CSS.
+ * 
+ * You can render this icon in a choice of colours: black or white.
  */
 class SmallRightArrowIcon extends React.Component {
   constructor(props) {
@@ -10,7 +13,12 @@ class SmallRightArrowIcon extends React.Component {
   }
 
   render() {
-    const containerStyle = 'ajc-small-right-arrow-icon-container ajc-default';
+    let containerStyle = 'ajc-small-right-arrow-icon-container ajc-default';
+    if (this.props.colour === 'white') {
+      containerStyle += ' ajc-border-colour-white';
+    } else {
+      containerStyle += ' ajc-border-colour-black';
+    }
     return (
       <React.Fragment>
         <div role="presentation" className={containerStyle}></div>
@@ -18,4 +26,11 @@ class SmallRightArrowIcon extends React.Component {
     );
   }
 }
+SmallRightArrowIcon.propTypes = {
+  /** The colour of the small icon */
+  colour: PropTypes.oneOf([ 'white', 'black' ]),
+};
+SmallRightArrowIcon.defaultProps = {
+	colour: 'black'
+};
 export default SmallRightArrowIcon;
