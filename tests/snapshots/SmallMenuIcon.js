4e61645804240d362b15df6070669beb336a3d61
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-05 18:17:14 +0000

    add snapshot tests for small menu icon and small right arrow icon

---
+++ tests/snapshots/SmallMenuIcon.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { TestDev } from 'ajc-jest-enzyme';
+import { SmallMenuIcon } from '../../src';
+
+describe('SmallMenuIcon', () => {
+  describe('Default props and rendering - Component in black colour', () => {
+    let jsonSnapshot;
+
+    beforeAll(() => {
+      jsonSnapshot = TestDev.createSnapshot(
+        <React.Fragment>
+          <SmallMenuIcon />
+        </React.Fragment>
+      );
+    });
+    
+    it('verifies the snapshot for the component', () => {
+      expect(jsonSnapshot).toMatchSnapshot();
+    });
+  });
+
+  describe('Transferred props and rendering - Component in white colour', () => {
+    let jsonSnapshot;
+
+    beforeAll(() => {
+      jsonSnapshot = TestDev.createSnapshot(
+        <React.Fragment>
+          <SmallMenuIcon colour="white" />
+        </React.Fragment>
+      );
+    });
+
+    it('verifies the snapshot for the component', () => {
+      expect(jsonSnapshot).toMatchSnapshot();
+    });
+  });
+});
