c3f7ebabf8e511ae74ffde5b91698a51c7d6157e
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-06 11:53:20 +0000

    add snapshots for footer component

---
+++ tests/snapshots/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { TestDev } from 'ajc-jest-enzyme';
+import { Footer } from '../../src';
+
+describe('Footer', () => {
+  describe('Default props and rendering - Component with grey background', () => {
+    let jsonSnapshot;
+
+    beforeAll(() => {
+      jsonSnapshot = TestDev.createSnapshot(
+        <React.Fragment>
+          <Footer>
+            This is a test.
+          </Footer>
+        </React.Fragment>
+      );
+    });
+    
+    it('verifies the snapshot for the component', () => {
+      expect(jsonSnapshot).toMatchSnapshot();
+    });
+  });
+
+  describe('Transferred props and rendering - Component with red background', () => {
+    let jsonSnapshot;
+
+    beforeAll(() => {
+      jsonSnapshot = TestDev.createSnapshot(
+        <React.Fragment>
+          <Footer colour="red">
+            This is a test.
+          </Footer>
+        </React.Fragment>
+      );
+    });
+
+    it('verifies the snapshot for the component', () => {
+      expect(jsonSnapshot).toMatchSnapshot();
+    });
+  });
+});
