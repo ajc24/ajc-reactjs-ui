251a530d7afd79513bfefd1da7027a20a62b1233
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-07 20:08:12 +0000

    add main content heading component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export { default as Footer } from './ui-elements/Footer';
 export { default as Header } from './ui-elements/Header';
 export { default as HeaderLogo } from './ui-elements/HeaderLogo';
 export { default as HeaderTitleAndSubtitle } from './ui-elements/HeaderTitleAndSubtitle';
+export { default as MainContentHeading } from './ui-elements/MainContentHeading';
 export { default as MainContentPanel } from './ui-elements/MainContentPanel';
 export { default as Menu } from './ui-elements/Menu';
 export { default as SmallMenuIcon } from './ui-elements/SmallMenuIcon';
