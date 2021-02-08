bea6681334de546e1a85783ab13fd39ddaebc2de
Anthony Cox <anthonyjohnc@gmail.com>
2021-02-08 20:53:54 +0000

    add accessibility tests for main content page title component

--- tests/accessibility/MainContentPageTitle.js
+++ tests/accessibility/MainContentPageTitle.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { toHaveNoViolations } from 'jest-axe';
 import { AccessibilityDev } from 'ajc-accessibility';
 import { TestDev } from 'ajc-jest-enzyme';
-import { MainContentHeading } from '../../src';
+import { MainContentPageTitle } from '../../src';
 
-describe('MainContentHeading', () => {
+describe('MainContentPageTitle', () => {
   const testTimeout = AccessibilityDev.getMaximumTimeout();
 
   /* Extend the expect behaviour of jest */
@@ -16,7 +16,7 @@ describe('MainContentHeading', () => {
     beforeAll(async () => {
       const html = TestDev.mountHtmlTemplate(
         <div role="region">
-          <MainContentHeading pageHeading="Test Page Title" />
+          <MainContentPageTitle titleText="Test Page Title Text" />
         </div>
       );
       jestAxeResults = await AccessibilityDev.runJestAxe(html);
