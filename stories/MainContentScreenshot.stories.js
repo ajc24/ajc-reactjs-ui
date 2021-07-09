a574e018c07d33f761c3a132afaf7aa039353274
Anthony Cox <anthonyjohnc@gmail.com>
2021-07-09 19:04:49 +0100

    add story for main content panel screenshot component

---
+++ stories/MainContentScreenshot.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { MainContentScreenshot } from '../src';
+import testImage from './images/storybook-test-screenshot.png';
+
+export default {
+	component: MainContentScreenshot,
+	title: 'Components/MainContentScreenshot',
+};
+  
+const Template = args => {
+	return <React.Fragment>
+		<MainContentScreenshot {...args} />
+	</React.Fragment>;
+}
+
+export const Default = Template.bind({});
+Default.args = {
+	id: 'default-main-content-screenshot-id',
+  imageSrc: testImage,
+  caption: 'Captions such as this appear below the screenshot.'
+};
