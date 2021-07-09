375cc66505d627906f0cab3ab082228f59b5f897
Anthony Cox <anthonyjohnc@gmail.com>
2021-07-09 23:03:54 +0100

    add accessibility tests for main content screenshot component

---
+++ tests/accessibility/MainContentScreenshot.js
@@ -0,0 +1,49 @@
+import 'jsdom-global/register';
+import React from 'react';
+import { toHaveNoViolations } from 'jest-axe';
+import { AccessibilityDev } from 'ajc-accessibility';
+import { TestDev } from 'ajc-jest-enzyme';
+import { MainContentScreenshot } from '../../src';
+import testImage from '../../stories/images/storybook-test-screenshot.png';
+
+describe('Main Content Screenshot', () => {
+  /* Retrieve the timeout for jest-axe tests */
+  const testTimeout = AccessibilityDev.getMaximumTimeout();
+
+  /* Extend the expect behaviour of jest */
+  expect.extend(toHaveNoViolations);
+
+  describe('Default props and rendering - Image src as an import of image data', () => {
+    let jestAxeResults;
+
+    beforeAll(async () => {
+      const html = TestDev.mountHtmlTemplate(
+        <div role="banner">
+          <MainContentScreenshot id="test-screenshot-id" caption="Test caption text content" imageSrc={testImage} />
+        </div>
+      );
+      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+    }, testTimeout);
+
+    it('verifies the jest-axe accessibility standards for the component', () => {
+      expect(jestAxeResults).toHaveNoViolations();
+    });
+  });
+
+  describe('Default props and rendering - Image src as a string', () => {
+    let jestAxeResults;
+
+    beforeAll(async () => {
+      const html = TestDev.mountHtmlTemplate(
+        <div role="banner">
+          <MainContentScreenshot id="test-screenshot-id" caption="Test caption text content" imageSrc="../../stories/images/storybook-test-screenshot.png" />
+        </div>
+      );
+      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+    }, testTimeout);
+
+    it('verifies the jest-axe accessibility standards for the component', () => {
+      expect(jestAxeResults).toHaveNoViolations();
+    });
+  });
+});
