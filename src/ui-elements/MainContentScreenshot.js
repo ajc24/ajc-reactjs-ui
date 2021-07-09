238ac4951064efdc64d504dd506cd32d3073d90e
Anthony Cox <anthonyjohnc@gmail.com>
2021-07-09 19:02:00 +0100

    add main content panel screenshot component

--- src/ui-elements/MainContentScreenshot.js
+++ src/ui-elements/MainContentScreenshot.js
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 import '../styling/ui-styles.css';
 
 /**
- * The Screenshot component allows you to render a screenshot with a caption underneath on any page of a web application.
+ * The MainContentScreenshot component allows you to render a screenshot with a caption underneath on any page of a web application. This component is intended for rendering gaming
+ * or TV based screenshots within articles but can be used to render any desired image.
  * 
  * You can render any text content you wish as the caption text. This component will be rendered at a maximum width of 1349px with the caption text rendered in bold and
  * displayed centrally underneath the screenshot. This component will collapse in size to support smaller screen sizes such as mobile devices.
  * 
  * For best results this component is intended for use within the MainContentPanel component. 
  */
-class Screenshot extends React.Component {
+class MainContentScreenshot extends React.Component {
   constructor(props) {
     super(props);
     this.disableMouseDownEvent = this.disableMouseDownEvent.bind(this);
@@ -34,24 +35,28 @@ class Screenshot extends React.Component {
   }
 
   render() {
+    const containerStyling = 'ajc-screenshot-container ajc-text-black ajc-default';
+    const innerStyling = 'ajc-screenshot-image-inner';
     const imageStyling = 'ajc-screenshot-image';
     const captionStyling = 'ajc-screenshot-caption';
     return (
-      <React.Fragment>
-        <img id={this.props.id} className={imageStyling} src={this.props.imageSrc} role="presentation" onLoad={this.setImageLoaded} onMouseDown={this.disableMouseDownEvent} />
-        <p className={captionStyling}>
+      <div className={containerStyling}>
+        <div className={innerStyling}>
+          <img id={this.props.id} className={imageStyling} src={this.props.imageSrc} role="presentation" onLoad={this.setImageLoaded} onMouseDown={this.disableMouseDownEvent} />
+        </div>
+        <p id={`${this.props.id}-caption`} className={captionStyling}>
           {this.props.caption}
         </p>
-      </React.Fragment>
+      </div>
     );
   }
 }
-Screenshot.propTypes = {
-  /** The unique identifier for the header logo component */
+MainContentScreenshot.propTypes = {
+  /** The unique identifier for the screenshot component */
   id: PropTypes.string.isRequired,
   /** The caption to appear underneath the screenshot */
   caption: PropTypes.string.isRequired,
   /** The image to be displayed within the component. */
   imageSrc: PropTypes.oneOfType([ PropTypes.string, PropTypes.object ]).isRequired,
 };
-export default Screenshot;
+export default MainContentScreenshot;
