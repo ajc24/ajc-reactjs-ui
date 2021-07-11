c03854881036ef3725e9aabce96871b01f1d27c9
Anthony Cox <anthonyjohnc@gmail.com>
2021-07-11 19:59:59 +0100

    add accessibility tests for buttons list component

---
+++ tests/accessibility/ButtonsList.js
@@ -0,0 +1,97 @@
+import 'jsdom-global/register';
+import React from 'react';
+import { toHaveNoViolations } from 'jest-axe';
+import { AccessibilityDev } from 'ajc-accessibility';
+import { TestDev } from 'ajc-jest-enzyme';
+import { ButtonsList } from '../../src';
+
+describe('Buttons List', () => {
+  /* Create the test data for the menu */
+  const testTimeout = AccessibilityDev.getMaximumTimeout();
+
+  /* Extend the expect behaviour of jest */
+  expect.extend(toHaveNoViolations);
+
+  /* Set the data for use in the tests */
+  const singleButtonList = [
+    {
+      id: 'test-single-button-id',
+      onClick: jest.fn(),
+      title: 'Test Default Single Grey Button',
+      type: 'button',
+    }
+  ];
+  const singleSubmitButtonList = [
+    {
+      id: 'test-single-submit-button-id',
+      onClick: jest.fn(),
+      title: 'Test Submit Button',
+      type: 'submit',
+    }
+  ];
+  const twoButtonsList = [
+    {
+      id: 'test-button-1-id',
+      onClick: jest.fn(),
+      title: 'Test Button 1',
+      type: 'button',
+    },
+    {
+      id: 'test-button-2-id',
+      onClick: jest.fn(),
+      title: 'Test Button 2',
+      type: 'button',
+    }
+  ];
+
+  describe('Default props and rendering - Single grey button', () => {
+    let jestAxeResults;
+
+    beforeAll(async () => {
+      const html = TestDev.mountHtmlTemplate(
+        <div role="banner">
+          <ButtonsList buttonsList={singleButtonList} />
+        </div>
+      );
+      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+    }, testTimeout);
+
+    it('verifies the jest-axe accessibility standards for the component', () => {
+      expect(jestAxeResults).toHaveNoViolations();
+    });
+  });
+
+  describe('Transferred props and rendering - Two center aligned red buttons', () => {
+    let jestAxeResults;
+
+    beforeAll(async () => {
+      const html = TestDev.mountHtmlTemplate(
+        <div role="banner">
+          <ButtonsList colour="red" alignment="center-align" buttonsList={twoButtonsList} />
+        </div>
+      );
+      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+    }, testTimeout);
+
+    it('verifies the jest-axe accessibility standards for the component', () => {
+      expect(jestAxeResults).toHaveNoViolations();
+    });
+  });
+
+  describe('Transferred props and rendering - Single right aligned grey submit button', () => {
+    let jestAxeResults;
+
+    beforeAll(async () => {
+      const html = TestDev.mountHtmlTemplate(
+        <div role="banner">
+          <ButtonsList colour="grey" alignment="right-align" buttonsList={singleSubmitButtonList} />
+        </div>
+      );
+      jestAxeResults = await AccessibilityDev.runJestAxe(html);
+    }, testTimeout);
+
+    it('verifies the jest-axe accessibility standards for the component', () => {
+      expect(jestAxeResults).toHaveNoViolations();
+    });
+  });
+});
