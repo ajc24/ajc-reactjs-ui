d45f52dad72a1cc34eee2655ede6e2c8ce301485
Anthony Cox <anthonyjohnc@gmail.com>
2021-07-12 17:54:17 +0100

    add snapshot tests for the file uploader component

---
+++ tests/snapshots/FileUploader.js
@@ -0,0 +1,50 @@
+import 'jsdom-global/register';
+import React from 'react';
+import { TestDev } from 'ajc-jest-enzyme';
+import { FileUploader } from '../../src';
+
+describe('File Uploader', () => {
+  let componentDidMountSpy;
+
+  beforeAll(() => {
+    componentDidMountSpy = jest
+      .spyOn(FileUploader.prototype, 'componentDidMount')
+      .mockImplementation(() => {});
+  });
+
+  afterAll(() => {
+    componentDidMountSpy.mockRestore();
+  });
+
+  describe('Default props and rendering', () => {
+    let jsonSnapshot;
+
+    beforeAll(() => {
+      jsonSnapshot = TestDev.createSnapshot(
+        <React.Fragment>
+          <FileUploader id="default-file-uploader-id" />
+        </React.Fragment>
+      );
+    });
+
+    it('verifies the snapshot for the component', () => {
+      expect(jsonSnapshot).toMatchSnapshot();
+    });
+  });
+
+  describe('Transferred props and rendering - File upload with red buttons, custom title text and additional upper spacing', () => {
+    let jsonSnapshot;
+
+    beforeAll(() => {
+      jsonSnapshot = TestDev.createSnapshot(
+        <React.Fragment>
+          <FileUploader id="custom-file-uploader-id" addUpperSpacing={true} colour="red" titleTextContent="Custom File Uploader Title:" />
+        </React.Fragment>
+      );
+    });
+
+    it('verifies the snapshot for the component', () => {
+      expect(jsonSnapshot).toMatchSnapshot();
+    });
+  });
+});
